059cec164383bdb18ab796e274c291c304304866
Vladislav <kononov.vw@gmail.com>
2022-09-22 16:24:09 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import secrets
+from sys import argv
+from eth_account import Account
+from datetime import datetime
+
+script, num_of_wallets = argv
+
+now = datetime.now().strftime('%d-%m-%Y %H-%M-%S')
+
+for _ in range(int(num_of_wallets)):
+    private_key = "0x" + secrets.token_hex(32)
+    account = Account.from_key(private_key)
+
+    with open(f"wallets {now}.txt", "a+") as file:
+        file.write(f"{account.address} {private_key} \n")
+
+print("Walllets created")
